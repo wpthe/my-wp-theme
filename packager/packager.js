98d90157bd3788a4832c9794b49eaeb9de0962c0
Dmitry Pripa <i@dpripa.com>
2021-03-12 14:51:05 +0200

    small packager improvements

--- packager/packager.js
+++ packager/packager.js
@@ -4,8 +4,8 @@ const log = require( 'log-beautify' );
 const config = require( './config' );
 
 function setMainFileVersion() {
-	if ( fs.existsSync( config.mainFile.file ) ) {
-		fs.readFile( config.mainFile.file, 'utf8', function( err, data ) {
+	if ( fs.existsSync( config.basePath + config.mainFile.file ) ) {
+		fs.readFile( config.basePath + config.mainFile.file, 'utf8', function( err, data ) {
 			if ( err ) {
 				return console.error( err );
 			}
@@ -25,7 +25,7 @@ function setPackageVersion( path ) {
 	if ( path && fs.existsSync( path ) ) {
 		const json = require( path );
 		json.version = config.version;
-		fs.writeFile( path, JSON.stringify( json, null, 2 ), function( err ) {
+		fs.writeFile( path, JSON.stringify( json, null, 2 ), 'utf8', function( err ) {
 			if ( err ) {
 				return console.error( err );
 			}
@@ -33,19 +33,14 @@ function setPackageVersion( path ) {
 	}
 }
 
-function createArchive( sources ) {
-	const sourcesPostfix = sources ? '-sources' : '';
-	const output = fs.createWriteStream( config.output + config.name + sourcesPostfix + '.zip' );
+function createArchive() {
+	const output = fs.createWriteStream( config.basePath + config.name + '.zip' );
 	const archive = archiver( 'zip', {});
 
 	output.on( 'close', function() {
-		if ( ! sources ) {
-			console.log( '\n' );
-			log.success_( '"' + config.name + sourcesPostfix + '.zip" released under version ' + config.version + ' to the theme folder.' );
-		} else {
-			log.success_( '"' + config.name + sourcesPostfix + '.zip" was placed near.' );
-			console.log( '\n' )
-		}
+		console.log( '\n' );
+		log.success_( '"' + config.name + '.zip" released under version ' + config.version + ' to the plugin folder.' );
+		console.log( '\n' )
 	});
 
 	archive.on( 'error', function( err ) {
@@ -55,22 +50,19 @@ function createArchive( sources ) {
 	archive.pipe( output );
 
 	let directories = config.directories;
-	if ( sources ) directories = directories.concat( config.sources.directories );
 	for ( let i = 0; i < directories.length; i++ ) {
-		archive.directory( '../' + directories[i], config.name + '/' + directories[i], null );
+		archive.directory( config.basePath + directories[i], config.name + '/' + directories[i], null );
 	}
 
 	let files = config.files;
-	if ( sources ) files = files.concat( config.sources.files );
 	for ( let i = 0; i < files.length; i++ ) {
-		archive.file( '../' + files[i], { name: config.name + '/' + files[i] });
+		archive.file( config.basePath + files[i], { name: config.name + '/' + files[i] });
 	}
 
 	archive.finalize();
 }
 
 setMainFileVersion();
-setPackageVersion( config.sources.packageJson );
-setPackageVersion( config.sources.composerJson );
-createArchive( false );
-createArchive( true );
+setPackageVersion( config.packageJson );
+setPackageVersion( config.composerJson );
+createArchive();
