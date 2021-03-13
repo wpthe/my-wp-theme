75e84d4775826e9e462863f05a4b430d36487045
Dmitry Pripa <i@dpripa.com>
2021-03-13 05:20:31 +0200

    Update Templates.php

--- includes/Templates.php
+++ includes/Templates.php
@@ -10,18 +10,18 @@ class Templates {
 		return is_page_template( 'templates/' . $slug . '.php' );
 	}
 
-	public static function get_part( string $slug = '', string $name = '', string $postfix = '', array $args = array() ) {
-		$slug    = $slug && $name ? $slug . '-' : $slug;
-		$postfix = $postfix ? '-' . $postfix : '';
+	public static function get_part( string $slug = '', string $name = '', string $addition = '', array $args = array() ) {
+		$slug     = $slug && $name ? $slug . '-' : $slug;
+		$addition = $addition ? '-' . $addition : '';
 
-		get_template_part( 'template-parts/' . $slug . $name . $postfix, null, $args );
+		get_template_part( 'template-parts/' . $slug . $name . $addition, null, $args );
 	}
 
-	public static function get_card( string $type = 'post', string $postfix = '', array $args = array() ) {
-		self::get_part( $type, 'card', $postfix, $args );
+	public static function get_card( string $type = 'post', string $addition = '', array $args = array() ) {
+		self::get_part( $type, 'card', $addition, $args );
 	}
 
-	public static function get_content( string $type = 'post', string $postfix = '', array $args = array() ) {
-		self::get_part( $type, 'content', $postfix, $args );
+	public static function get_content( string $type = 'post', string $addition = '', array $args = array() ) {
+		self::get_part( $type, 'content', $addition, $args );
 	}
 }
